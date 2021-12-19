4981aa5ec33d6296467b9e13bc60e87ff7b5411b
Jakub Zajda <jakubzajda@gmail.com>
2021-12-19 10:26:07 +0100

    Initial Commit

---
+++ DiceGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceGame
+//
+//  Created by Kamila Mroziewska on 19/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Kamila Mroziewska on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
